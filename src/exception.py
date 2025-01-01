02f52f3d7352a4074dc8baaed3530a63bb5f030c
Aman Gupta <guptaaman89630@gmail.com>
2025-01-02 00:44:30 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+sys.path.append('E:/new git/mlproject')
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -14,5 +15,11 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
+'''if __name__=="__main__":
+        try:
+            a=1/0
+        except Exception as e:
+            logging.info("Divide by zero")
+            raise CustomException(e,sys)'''
 
 
